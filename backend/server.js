22b5e4314a6923999df56d598ba3ef4928f7224b
HanaPark <hn946002@dal.ca>
2021-06-09 10:26:48 -0300

    change server.js

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,27 @@
+require('./database');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const path = require('path')
 const app = express();
-require('./database');
-    app.use(bodyParser.json());
-app.use(cors());
-// API
-const users = require('./api/user');
-app.use('/api/users', users);
 const port = process.env.PORT || 5000;
+//models
+const users = require('./api/user');
+
+//middleware
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/api/*', users);
+
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('build'));
+}
+app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "./build", "index.html"));
+});
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
+
+
