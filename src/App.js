a12f020220c1ea85117a61875e19d0c8e8cef860
HanaPark <hn946002@dal.ca>
2021-07-03 12:14:16 -0300

    change forlder structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import './App.css';
+import './assets/css/App.css';
 import {Home, SignIn, Users} from "./pages";
 
 function App() {
