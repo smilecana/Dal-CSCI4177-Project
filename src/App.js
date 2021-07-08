870c7f0225718a99f60372f2a7ceee0656e25270
HanaPark <hn946002@dal.ca>
2021-07-08 18:12:20 -0300

    Revert "change forlder structure"
    This reverts commit a12f020220c1ea85117a61875e19d0c8e8cef860.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import './assets/css/App.css';
+import './App.css';
 import {Home, SignIn, Users} from "./pages";
 
 function App() {
