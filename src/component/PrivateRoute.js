0289b8cff0f30d314114251531ec09b15ffc802b
HanaPark <hn946002@dal.ca>
2021-07-22 03:23:01 -0300

    no message

--- src/component/PrivateRoute.js
+++ src/component/PrivateRoute.js
@@ -5,6 +5,7 @@ import authentication from "./authentication";
 const PrivateRoute = ({component: Component, auth, ...rest}) => {
     return (<Route {...rest}
                    render={(props) => {
+                       console.log(props);
                        return (authentication() && (Object.keys(authentication()).length !== 0)) ? <Component {...props} data={auth} /> : <Redirect to="/login"/>
                    }}/>)
 }
