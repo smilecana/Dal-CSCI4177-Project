02c8df66e04d8cd3c38bf989d94cd3a262118271
HanaPark <hn946002@dal.ca>
2021-07-22 01:40:36 -0300

    modified userprofile

--- src/component/authentication.js
+++ src/component/authentication.js
@@ -1,6 +1,7 @@
 const jsonwebtoken = require('jsonwebtoken');
 
 const authentication = auth => {
+    console.log(auth);
     if (localStorage.getItem('lmsToken')) {
         auth = jsonwebtoken.verify(localStorage.getItem('lmsToken'), 'lmsPlatform')
         return auth;
